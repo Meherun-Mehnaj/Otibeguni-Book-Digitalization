21dfe8391bb41a09cf7b1139c898dc19fd83fc53
Meherun Mehnaj <148745277+Meherun-Mehnaj@users.noreply.github.com>
2025-04-18 15:54:43 +0600

    Add files via upload

---
+++ Banglar Kingbodonti 2/PDF to Text/extract_banglarKingbodonti.py
@@ -0,0 +1,20 @@
+import pytesseract
+from PIL import Image
+from pdf2image import convert_from_path
+
+print("📄 Converting PDF to images")
+pages = convert_from_path("F:\Otibeguni\Banglar Kingbodonti 2.pdf")
+
+output_filename = "banglar_kingbodonti2.txt"
+with open(output_filename, "w", encoding="utf-8") as output_file:
+    for i, page in enumerate(pages):
+        print(f"OCR on page {i+1}")
+        text = pytesseract.image_to_string(page, lang='ben')
+
+        # Write page separator and text
+        output_file.write(f"\n\n--- 📄 Page {i+1} ---\n\n")
+        output_file.write(text)
+
+        print(f"✅ Added page {i+1} to {output_filename}")
+
+print("🎉 All done! Combined text saved in one file.")
